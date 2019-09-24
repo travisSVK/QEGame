c5a81b80f8ab22570afdf33ab956a6ed0dfd9d4a
travisko <marek.cern@gmail.com>
2019-09-24 21:01:16 +0200

    Removed unity generated comments from scripts. Test scene added to gitignore.

--- QEGame/Assets/Scripts/HorizontalMovementController.cs
+++ QEGame/Assets/Scripts/HorizontalMovementController.cs
@@ -8,14 +8,12 @@ public class HorizontalMovementController : MonoBehaviour
 
     private CharacterController characterController;
     private Vector3 moveDirection = Vector3.zero;
-
-    // Start is called before the first frame update
+    
     void Start()
     {
         characterController = GetComponent<CharacterController>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         moveDirection = new Vector3(Input.GetAxis("Horizontal") * movementSpeed, 0.0f, 0.0f);
--- QEGame/Assets/Scripts/VerticalPlayerController.cs
+++ QEGame/Assets/Scripts/VerticalPlayerController.cs
@@ -8,14 +8,12 @@ public class VerticalPlayerController : MonoBehaviour
 
     private CharacterController characterController;
     private Vector3 moveDirection = Vector3.zero;
-
-    // Start is called before the first frame update
+    
     void Start()
     {
         characterController = GetComponent<CharacterController>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         moveDirection = new Vector3(0.0f, 0.0f, Input.GetAxis("Vertical") * movementSpeed);
