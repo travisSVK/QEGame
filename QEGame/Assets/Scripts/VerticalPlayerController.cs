7a35f45910fd2c57709bbb3c8f8c736a55950f07
Marek Cernak <marek.cern@gmail.com>
2019-09-25 13:08:48 +0200

    Added movement controllers for both players (horizontal and vertical)

---
+++ QEGame/Assets/Scripts/VerticalPlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalPlayerController : MonoBehaviour
+{
+    public float movementSpeed = 5.0f;
+
+    private CharacterController characterController;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        moveDirection = new Vector3(0.0f, 0.0f, Input.GetAxis("Vertical") * movementSpeed);
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
