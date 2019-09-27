bd89fbba604a17e0dc3de70f8e4fce74d0c1aaa1
travisko <marek.cern@gmail.com>
2019-09-27 10:03:56 +0200

    Added modifiers for unity generated methods. Added comments to public methods.

--- QEGame/Assets/Scripts/HorizontalPlayerController.cs
+++ QEGame/Assets/Scripts/HorizontalPlayerController.cs
@@ -9,18 +9,21 @@ public class HorizontalPlayerController : MonoBehaviour
     private CharacterController _characterController;
     private Vector3 _moveDirection = Vector3.zero;
 
+    /**
+    * @brief Speed of the game object horizontal movement.
+    */
     public float MovementSpeed
     {
         get { return _movementSpeed; }
         set { _movementSpeed = value; }
     }
 
-    void Start()
+    private void Start()
     {
         _characterController = GetComponent<CharacterController>();
     }
 
-    void Update()
+    private void Update()
     {
         _moveDirection = new Vector3(Input.GetAxis("Horizontal") * _movementSpeed, 0.0f, 0.0f);
         _characterController.Move(_moveDirection * Time.deltaTime);
--- QEGame/Assets/Scripts/VerticalPlayerController.cs
+++ QEGame/Assets/Scripts/VerticalPlayerController.cs
@@ -9,18 +9,21 @@ public class VerticalPlayerController : MonoBehaviour
     private CharacterController _characterController;
     private Vector3 _moveDirection = Vector3.zero;
 
+    /**
+    * @brief Speed of the game object vertical movement.
+    */
     public float MovementSpeed
     {
         get { return _movementSpeed; }
         set { _movementSpeed = value; }
     }
 
-    void Start()
+    private void Start()
     {
         _characterController = GetComponent<CharacterController>();
     }
 
-    void Update()
+    private void Update()
     {
         _moveDirection = new Vector3(0.0f, 0.0f, Input.GetAxis("Vertical") * _movementSpeed);
         _characterController.Move(_moveDirection * Time.deltaTime);
