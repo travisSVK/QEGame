1ee9e77249daa3612e9c1c6a941b905f249a4088
Marek Cernak <marek.cern@gmail.com>
2019-10-01 09:38:05 +0200

    Script changes in accordance with guidelines. (#17)
    * Added horizontal character controller. Added personal test scene.
    
    * Added vertical player controller.
    
    * Removed unity generated comments from scripts. Test scene added to gitignore.
    
    * Test scene added to gitignore.
    
    * Test scene added to gitignore.
    
    * Unified test scene name in gitignore.
    
    * Fix
    
    * Fix name
    
    * Changed usage of editable member variable as per coding guidelines.
    
    * Changed usage of editable member variable as per coding guidelines.
    
    * Added underscores to private variables.
    
    * Added modifiers for unity generated methods. Added comments to public methods.
    
    * Added new folder for player specific scripts.
    
    * Update HorizontalPlayerController.cs
    
    * Update VerticalPlayerController.cs

---
+++ QEGame/Assets/Scripts/Player/VerticalPlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalPlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float _movementSpeed = 5.0f;
+    private CharacterController _characterController;
+    private Vector3 _moveDirection = Vector3.zero;
+
+    /**
+     * @brief Speed of the game object vertical movement.
+     */
+    public float MovementSpeed
+    {
+        get { return _movementSpeed; }
+        set { _movementSpeed = value; }
+    }
+
+    private void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        _moveDirection = new Vector3(0.0f, 0.0f, Input.GetAxis("Vertical") * _movementSpeed);
+        _characterController.Move(_moveDirection * Time.deltaTime);
+    }
+}
