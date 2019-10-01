35bef1a187d204cd633db4c4ed61cfe23b7d4c76
ludvigarlebrink <ludvig.arlebrink@outlook.com>
2019-10-01 10:55:21 +0200

    Camera movement

---
+++ QEGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Serializable]
+    public struct ControlPoint
+    {
+        public string name;
+        public Vector3 position;
+        public Quaternion rotation;
+
+        public ControlPoint(string name, Vector3 position, Quaternion rotation)
+        {
+            this.name = name;
+            this.position = position;
+            this.rotation = rotation;
+        }
+    }
+
+    [SerializeField]
+    private float _speed = 5.0f;
+
+    [SerializeField]
+    private List<ControlPoint> _controlPoints = new List<ControlPoint>();
+
+    private bool _isMovingForward = true;
+    private int _currentControlPoint = 0;
+    private float _movementProgress = 0.0f;
+
+    public float speed
+    {
+        get { return _speed; }
+        set { _speed = value; }
+    }
+
+    public List<ControlPoint> controlPoints
+    {
+        get { return _controlPoints; }
+        set { _controlPoints = value; }
+    }
+
+    public bool canMoveBack
+    {
+        get
+        {
+            return _currentControlPoint - 1 >= 0;
+        }
+    }
+
+    public bool canMoveForward
+    {
+        get
+        {
+            return _currentControlPoint + 1 < _controlPoints.Count;
+        }
+    }
+
+    /**
+     * @brief Move to the previous control point.
+     */
+    public void MoveBack()
+    {
+        // If enabled, currently moving.
+        if (enabled)
+        {
+            SnapToNextControlPoint();
+        }
+
+        if (canMoveBack)
+        {
+            enabled = true;
+            _isMovingForward = false;
+            _movementProgress = 0.0f;
+        }
+    }
+
+    /**
+     * @brief Move to the next control point.
+     */
+    public void MoveForward()
+    {
+        // If enabled, currently moving.
+        if (enabled)
+        {
+            SnapToNextControlPoint();
+        }
+
+        if (canMoveForward)
+        {
+            enabled = true;
+            _isMovingForward = true;
+            _movementProgress = 0.0f;
+        }
+    }
+
+    public void Restart()
+    {
+        _currentControlPoint = 0;
+        UpdateTransformations();
+    }
+
+    public void SnapBack()
+    {
+        if (canMoveBack)
+        {
+            --_currentControlPoint;
+            UpdateTransformations();
+        }
+    }
+
+    public void SnapForward()
+    {
+        if (canMoveForward)
+        {
+            ++_currentControlPoint;
+            UpdateTransformations();
+        }
+    }
+
+    /**
+     * @brief Update the position and position in accordance to the current control point.
+     * 
+     * Call this function from the editor only since the update function is not called.
+     */
+    public void UpdateTransformations()
+    {
+        transform.position = _controlPoints[_currentControlPoint].position;
+        transform.rotation = _controlPoints[_currentControlPoint].rotation;
+    }
+
+    private void SnapToNextControlPoint()
+    {
+        if (_isMovingForward)
+        {
+            _currentControlPoint = Mathf.Min(_controlPoints.Count - 1, _currentControlPoint + 1);
+        }
+        else
+        {
+            _currentControlPoint = Mathf.Max(0, _currentControlPoint - 1);
+        }
+
+        transform.position = _controlPoints[_currentControlPoint].position;
+        enabled = false;
+    }
+
+    private void LateUpdate()
+    {
+        if (_isMovingForward)
+        {
+            _movementProgress += (Time.deltaTime / Vector3.Distance(_controlPoints[_currentControlPoint].position, _controlPoints[_currentControlPoint + 1].position)) * _speed;
+
+            if (_movementProgress >= 1.0f)
+            {
+                ++_currentControlPoint;
+                enabled = false;
+                transform.position = _controlPoints[_currentControlPoint].position;
+                transform.rotation = _controlPoints[_currentControlPoint].rotation;
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(_controlPoints[_currentControlPoint].position, _controlPoints[_currentControlPoint + 1].position, Mathf.SmoothStep(0.0f, 1.0f, _movementProgress));
+                transform.rotation = Quaternion.Slerp(_controlPoints[_currentControlPoint].rotation, _controlPoints[_currentControlPoint + 1].rotation, Mathf.SmoothStep(0.0f, 1.0f, _movementProgress));
+            }
+        }
+        else
+        {
+            _movementProgress += (Time.deltaTime / Vector3.Distance(_controlPoints[_currentControlPoint].position, _controlPoints[_currentControlPoint - 1].position)) * _speed;
+
+            if (_movementProgress >= 1.0f)
+            {
+                --_currentControlPoint;
+                enabled = false;
+                transform.position = _controlPoints[_currentControlPoint].position;
+                transform.rotation = _controlPoints[_currentControlPoint].rotation;
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(_controlPoints[_currentControlPoint].position, _controlPoints[_currentControlPoint - 1].position, Mathf.SmoothStep(0.0f, 1.0f, _movementProgress));
+                transform.rotation = Quaternion.Slerp(_controlPoints[_currentControlPoint].rotation, _controlPoints[_currentControlPoint - 1].rotation, Mathf.SmoothStep(0.0f, 1.0f, _movementProgress));
+            }
+        }
+    }
+
+    private void Start()
+    {
+        if (_controlPoints.Count > 0)
+        {
+            transform.position = _controlPoints[0].position;
+            transform.rotation = _controlPoints[0].rotation;
+        }
+
+        enabled = false;
+    }
+}
---
+++ QEGame/Assets/Scripts/Editor/CameraControllerInspector.cs
@@ -0,0 +1,205 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CameraController))]
+public class CameraControllerInspector : Editor
+{
+    private enum Mode
+    {
+        Translate,
+        Rotate
+    }
+
+    private Mode _mode = Mode.Translate;
+
+    private List<bool> _foldouts = new List<bool>();
+
+    private static readonly string[] _modes = { "Translate", "Rotate" };
+
+    public override void OnInspectorGUI()
+    {
+        CameraController cameraController = (CameraController)target;
+        if (cameraController.controlPoints.Count != _foldouts.Count)
+        {
+            ResetFoldout(cameraController);
+        }
+
+        EditorGUILayout.LabelField("Properties", EditorStyles.boldLabel);
+
+        cameraController.speed = EditorGUILayout.FloatField("Speed", cameraController.speed);
+
+        EditorGUILayout.LabelField("Mode", EditorStyles.boldLabel);
+
+        _mode = (Mode)GUILayout.SelectionGrid((int)_mode, _modes, 2);
+
+        EditorGUILayout.LabelField("Control Points", EditorStyles.boldLabel);
+
+        for (int i = 0; i < cameraController.controlPoints.Count; ++i)
+        {
+            _foldouts[i] = EditorGUILayout.BeginFoldoutHeaderGroup(_foldouts[i], cameraController.controlPoints[i].name);
+            if (_foldouts[i])
+            {
+                string name = EditorGUILayout.TextField("Name", cameraController.controlPoints[i].name);
+                Vector3 position = EditorGUILayout.Vector3Field("Position", cameraController.controlPoints[i].position);
+                Quaternion rotation = Quaternion.Euler(EditorGUILayout.Vector3Field("Rotation", cameraController.controlPoints[i].rotation.eulerAngles));
+                cameraController.controlPoints[i] = new CameraController.ControlPoint(name, position, rotation);
+
+                GUILayout.BeginHorizontal();
+                {
+                    EditorGUI.BeginDisabledGroup(i == 0);
+                    {
+                        if (GUILayout.Button("Up"))
+                        {
+                            CameraController.ControlPoint temp = cameraController.controlPoints[i];
+                            cameraController.controlPoints[i] = cameraController.controlPoints[i - 1];
+                            cameraController.controlPoints[i - 1] = temp;
+                            GUILayout.EndHorizontal();
+                            break;
+                        }
+                    }
+                    EditorGUI.EndDisabledGroup();
+
+                    EditorGUI.BeginDisabledGroup(i >= cameraController.controlPoints.Count - 1);
+                    {
+                        if (GUILayout.Button("Down"))
+                        {
+                            CameraController.ControlPoint temp = cameraController.controlPoints[i];
+                            cameraController.controlPoints[i] = cameraController.controlPoints[i + 1];
+                            cameraController.controlPoints[i + 1] = temp;
+                            GUILayout.EndHorizontal();
+                            break;
+                        }
+                    }
+                    EditorGUI.EndDisabledGroup();
+
+                    if (GUILayout.Button("Delete"))
+                    {
+                        cameraController.controlPoints.RemoveAt(i);
+                        GUILayout.EndHorizontal();
+                        break;
+                    }
+                }
+                GUILayout.EndHorizontal();
+            }
+            EditorGUILayout.EndFoldoutHeaderGroup();
+        }
+
+        EditorGUILayout.Space();
+
+        if (GUILayout.Button("Add Control Point"))
+        {
+            if (cameraController.controlPoints.Count <= 0)
+            {
+                cameraController.controlPoints.Add(new CameraController.ControlPoint("New Control Point", Vector3.zero, Quaternion.identity));
+            }
+            else if (cameraController.controlPoints.Count == 1)
+            {
+                cameraController.controlPoints.Add(new CameraController.ControlPoint("New Control Point", new Vector3(0.0f, 0.0f, 5.0f), Quaternion.identity));
+            }
+            else
+            {
+                Vector3 normal = cameraController.controlPoints[cameraController.controlPoints.Count - 1].position - cameraController.controlPoints[cameraController.controlPoints.Count - 2].position;
+                cameraController.controlPoints.Add(new CameraController.ControlPoint("New Control Point", cameraController.controlPoints[cameraController.controlPoints.Count - 1].position + normal.normalized * 5.0f, Quaternion.identity));
+            }
+            _foldouts.Add(true);
+        }
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Controls", EditorStyles.boldLabel);
+
+        GUILayout.BeginHorizontal();
+        {
+            EditorGUI.BeginDisabledGroup(!cameraController.canMoveForward);
+            {
+                if (GUILayout.Button("Move Forward"))
+                {
+                    if (EditorApplication.isPlaying)
+                    {
+                        cameraController.MoveForward();
+                    }
+                    else
+                    {
+                        cameraController.SnapForward();
+                    }
+                }
+            }
+            EditorGUI.EndDisabledGroup();
+
+            EditorGUI.BeginDisabledGroup(!cameraController.canMoveBack);
+            {
+                if (GUILayout.Button("Move Back"))
+                {
+                    if (EditorApplication.isPlaying)
+                    {
+                        cameraController.MoveBack();
+                    }
+                    else
+                    {
+                        cameraController.SnapBack();
+                    }
+                }
+            }
+            EditorGUI.EndDisabledGroup();
+        }
+        GUILayout.EndHorizontal();
+    }
+
+    protected virtual void OnSceneGUI()
+    {
+        CameraController cameraController = (CameraController)target;
+
+        Handles.color = Color.cyan;
+        for (int i = 0; i < cameraController.controlPoints.Count - 1; ++i)
+        {
+            Handles.DrawLine(cameraController.controlPoints[i].position, cameraController.controlPoints[i + 1].position);
+        }
+
+        GUIStyle textStyle = new GUIStyle();
+        textStyle.normal.textColor = Color.white;
+
+        for (int i = 0; i < cameraController.controlPoints.Count; ++i)
+        {
+            Handles.Label(cameraController.controlPoints[i].position, cameraController.controlPoints[i].name, textStyle);
+
+            if (_mode == Mode.Translate)
+            {
+                EditorGUI.BeginChangeCheck();
+                Vector3 newTargetPosition = Handles.PositionHandle(cameraController.controlPoints[i].position, Quaternion.identity);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    cameraController.controlPoints[i] = new CameraController.ControlPoint(cameraController.controlPoints[i].name, newTargetPosition, cameraController.controlPoints[i].rotation);
+                }
+            }
+            else
+            {
+                EditorGUI.BeginChangeCheck();
+                Quaternion rotation = Handles.RotationHandle(cameraController.controlPoints[i].rotation, cameraController.controlPoints[i].position);
+                cameraController.controlPoints[i] = new CameraController.ControlPoint(cameraController.controlPoints[i].name, cameraController.controlPoints[i].position, rotation);
+                cameraController.UpdateTransformations();
+            }
+        }
+    }
+
+    private void ResetFoldout(CameraController cameraController)
+    {
+        _foldouts.Clear();
+        for (int i = 0; i < cameraController.controlPoints.Count; ++i)
+        {
+            _foldouts.Add(true);
+        }
+    }
+
+    private void OnEnable()
+    {
+        CameraController cameraController = (CameraController)target;
+        if (cameraController.controlPoints.Count != _foldouts.Count)
+        {
+            ResetFoldout(cameraController);
+        }
+
+        cameraController.Restart();
+    }
+}
