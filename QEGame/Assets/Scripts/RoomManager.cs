3d9f221a2a99c71513d5b2ac2a207bc724b14c0a
ludvigarlebrink <ludvig.arlebrink@outlook.com>
2019-10-07 15:23:31 +0200

    Scene Manager (#24)

---
+++ QEGame/Assets/Scripts/RoomManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<string> _rooms = new List<string>();
+
+    private List<Scene> _loadedScenes = new List<Scene>();
+
+    public List<string> rooms
+    {
+        get { return _rooms; }
+        set { _rooms = value; }
+    }
+
+    public void Refresh()
+    {
+        for (int i = 0; i < _loadedScenes.Count; ++i)
+        {
+            SceneManager.UnloadSceneAsync(_loadedScenes[i]);
+        }
+
+        _loadedScenes.Clear();
+
+        for (int i = 0; i < _rooms.Count; ++i)
+        {
+            SceneManager.LoadScene(_rooms[i], LoadSceneMode.Additive);
+            _loadedScenes.Add(SceneManager.GetSceneByName(_rooms[i]));
+        }
+    }
+
+    private void Awake()
+    {
+        Refresh();
+    }
+}
