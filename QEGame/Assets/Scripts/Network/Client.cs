e91564adf8dbc56d78cf7e2c7293b946dfd717d1
kralsud <kralsud@protonmail.com>
2019-10-07 16:46:49 +0200

    Update

--- QEGame/Assets/Scripts/Network/Client.cs
+++ QEGame/Assets/Scripts/Network/Client.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
 
 public class Client : MonoBehaviour
@@ -37,7 +36,7 @@ public class Client : MonoBehaviour
             msg.x = transform.position.x;
             msg.y = transform.position.y;
             msg.z = transform.position.z;
-            msg.initialized = true;
+            msg.isInitialized = true;
             StateObject state = new StateObject();
             state.workSocket = _sender;
             Send<Message>(state, msg);
@@ -50,18 +49,18 @@ public class Client : MonoBehaviour
         IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
         IPAddress ipAddr = ipHost.AddressList[0];
         IPEndPoint remoteEndPoint = new IPEndPoint(ipAddr, 11111);
-        // Creation TCP/IP Socket
         _sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
         try
         {
-            // Connect to the remote endpoint.  
+            // Connect to the remote endpoint.
             _sender.BeginConnect(remoteEndPoint,
                 new AsyncCallback(ConnectCallback), _sender);
             _connectDone.WaitOne();
 
             _sendDone.Reset();
             _receiveDone.Reset();
-            //TODO send id here
+            // TODO send ID here.
             MessageConnected msg = new MessageConnected();
             msg.connected = true;
             msg.clientId = clientId;
@@ -70,7 +69,7 @@ public class Client : MonoBehaviour
             Send(state, msg);
             _sendDone.WaitOne();
 
-            // Receive the response ack from the remote device.  
+            // Receive the ACK from the remote device.
             ReceiveAck(_sender);
             _receiveDone.WaitOne();
         }
@@ -86,13 +85,12 @@ public class Client : MonoBehaviour
     {
         while (true)
         {
-            // TODO add other message types processing here (that memans probably adding wrapper 
-            // around the generic message stating which type of message it is (using enum) and adding a switch here)
+            // TODO add other message types processing here (that probably means adding wrappers around the
+            // generic message stating which type of message it is (using enum) and adding a switch here).
             Message msg = GetMessage();
-            if (msg.initialized)
+            if (msg.isInitialized)
             {
-                // TODO change the position of the other player (if tracking, if not, just remove receiving this message)
-                Debug.Log("id: " + msg.clientId + " Position: " + new Vector3(msg.x, msg.y, msg.z));
+                transform.position = new Vector3(msg.x, msg.y, msg.z);
             }
         }
     }
@@ -100,12 +98,12 @@ public class Client : MonoBehaviour
     private Message GetMessage()
     {
         Message msg = new Message();
-        msg.initialized = false;
+        msg.isInitialized = false;
         _messageQueueMutex.WaitOne();
         if (_messageQueue.Count != 0)
         {
             msg = _messageQueue.Dequeue();
-            msg.initialized = true;
+            msg.isInitialized = true;
         }
         _messageQueueMutex.ReleaseMutex();
         return msg;
@@ -115,15 +113,15 @@ public class Client : MonoBehaviour
     {
         try
         {
-            // Retrieve the socket from the state object.  
+            // Retrieve the socket from the state object.
             Socket sender = (Socket)ar.AsyncState;
 
-            // Complete the connection.  
+            // Complete the connection.
             sender.EndConnect(ar);
 
             Debug.Log("Socket connected to " + sender.RemoteEndPoint.ToString());
-            
-            // Signal that the connection has been made.  
+
+            // Signal that the connection has been made.
             _connectDone.Set();
         }
         catch (Exception e)
@@ -136,12 +134,12 @@ public class Client : MonoBehaviour
     {
         try
         {
-            // Create the state object.  
+            // Create the state object.
             StateObject state = new StateObject();
             state.workSocket = client;
 
-            // Begin receiving the data from the remote device.  
-            client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+            // Begin receiving the data from the remote device.
+            client.BeginReceive(state.buffer, 0, StateObject.BUFFER_SIZE, 0,
                 new AsyncCallback(ReceiveAckCallback), state);
         }
         catch (Exception e)
@@ -155,12 +153,12 @@ public class Client : MonoBehaviour
         String content = String.Empty;
         try
         {
-            // Retrieve the state object and the client socket   
-            // from the asynchronous state object.  
+            // Retrieve the state object and the client socket
+            // from the asynchronous state object.
             StateObject state = (StateObject)ar.AsyncState;
             Socket sender = state.workSocket;
 
-            // Read data from the remote device.  
+            // Read data from the remote device.
             int bytesRead = sender.EndReceive(ar);
 
             if (bytesRead > 0)
@@ -172,8 +170,8 @@ public class Client : MonoBehaviour
                     StateObject newState = new StateObject();
                     newState.workSocket = sender;
 
-                    // Begin receiving the data from the remote device.  
-                    sender.BeginReceive(newState.buffer, 0, StateObject.BufferSize, 0,
+                    // Begin receiving the data from the remote device.
+                    sender.BeginReceive(newState.buffer, 0, StateObject.BUFFER_SIZE, 0,
                         new AsyncCallback(ReceiveCallback), newState);
                 }
             }
@@ -188,12 +186,12 @@ public class Client : MonoBehaviour
     {
         try
         {
-            // Retrieve the state object and the client socket   
-            // from the asynchronous state object.  
+            // Retrieve the state object and the client socket
+            // from the asynchronous state object.
             StateObject state = (StateObject)ar.AsyncState;
             Socket sender = state.workSocket;
 
-            // Read data from the remote device.  
+            // Read data from the remote device.
             int bytesRead = sender.EndReceive(ar);
 
             if (bytesRead > 0)
@@ -203,8 +201,8 @@ public class Client : MonoBehaviour
                 _messageQueue.Enqueue(msg);
                 _messageQueueMutex.ReleaseMutex();
 
-                // Begin receiving the data from the remote device.  
-                sender.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                // Begin receiving the data from the remote device.
+                sender.BeginReceive(state.buffer, 0, StateObject.BUFFER_SIZE, 0,
                     new AsyncCallback(ReceiveCallback), state);
             }
         }
@@ -218,7 +216,7 @@ public class Client : MonoBehaviour
     {
         byte[] byteData = MessageUtils.Serialize<MessageType>(message);
 
-        // Begin sending the data to the remote device.  
+        // Begin sending the data to the remote device.
         state.workSocket.BeginSend(byteData, 0, byteData.Length, 0,
             new AsyncCallback(SendCallback), state);
     }
@@ -228,10 +226,12 @@ public class Client : MonoBehaviour
         try
         {
             StateObject state = (StateObject)ar.AsyncState;
-            // Complete sending the data to the remote device.  
+
+            // Complete sending the data to the remote device.
             int bytesSent = state.workSocket.EndSend(ar);
             Debug.Log("Sent " + bytesSent + " bytes to server.");
-            // Signal that all bytes have been sent.  
+
+            // Signal that all bytes have been sent.
             _sendDone.Set();
         }
         catch (Exception e)
