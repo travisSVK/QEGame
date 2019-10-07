7d2863297452a1467cef20a6f24d5b1be69c8dd3
travisko <marek.cern@gmail.com>
2019-10-07 15:58:32 +0200

    Implemented simple server-client communication with ACK when opening connection and one type of message including the position of the clients.

---
+++ QEGame/Assets/Scripts/Network/Message.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+[Serializable]
+public struct MessageConnected
+{
+    public bool connected;
+    public int clientId;
+}
+
+[Serializable]
+public struct Message
+{
+    public bool initialized;
+    public float x;
+    public float y;
+    public float z;
+    public int clientId;
+}
+
+public class MessageUtils
+{
+    public static MessageType Deserialize<MessageType>(byte[] buffer)
+    {
+        using (var memStream = new MemoryStream())
+        {
+            var binForm = new BinaryFormatter();
+            memStream.Write(buffer, 0, buffer.Length);
+            memStream.Seek(0, SeekOrigin.Begin);
+            MessageType message = (MessageType)binForm.Deserialize(memStream);
+            return message;
+        }
+    }
+
+    public static byte[] Serialize<MessageType>(MessageType message)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        using (var ms = new MemoryStream())
+        {
+            bf.Serialize(ms, message);
+            return ms.ToArray();
+        }
+    }
+}
