412191811f1db37d814955c66f887745571574a2
travisko <marek.cern@gmail.com>
2019-10-07 15:58:32 +0200

    Implemented simple server-client communication with ACK when opening connection and one type of message including the position of the clients.

---
+++ QEGame/Assets/Scripts/Network/StateObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+using System.Net.Sockets;
+
+public class StateObject
+{
+    // Client  socket.  
+    public Socket workSocket = null;
+    public Socket sendSocket = null;
+    // Size of receive buffer.  
+    public const int BufferSize = 1024;
+    // Receive buffer.  
+    public byte[] buffer = new byte[BufferSize];
+}
