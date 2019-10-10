8d284c1524c09081e8f528acf21f495832d1fa27
ludvigarlebrink <ludvig.arlebrink@outlook.com>
2019-10-10 13:39:17 +0200

    End scene

---
+++ QEGame/Assets/Scripts/EnableBasedOnLanguage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableBasedOnLanguage : MonoBehaviour
+{
+    public Language language;
+
+    private void Start()
+    {
+        LanguageManager languageManager = FindObjectOfType<LanguageManager>();
+        if (languageManager)
+        {
+            if (languageManager.activeLanguage == language)
+            {
+                gameObject.SetActive(true);
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            Debug.LogError("Could not find LanguageManager in the scene.");
+        }
+    }
+}
