e15eefe4173c79891c007c7daeec11bb3082dccd
kralsud <kralsud@protonmail.com>
2019-10-10 10:59:43 +0200

    Removed rebasing leftover

--- QEGame/Assets/Scripts/Player/VerticalPlayerController.cs
+++ QEGame/Assets/Scripts/Player/VerticalPlayerController.cs
@@ -6,30 +6,8 @@ public class VerticalPlayerController : PlayerControllerBase
 
     public virtual void Update()
     {
-<<<<<<< HEAD
-        get { return _movementSpeed; }
-        set { _movementSpeed = value; }
-    }
-
-    private void FixedUpdate()
-    {
-        _rigidBody.MovePosition(_rigidBody.position + _input * _movementSpeed * Time.fixedDeltaTime);
-    }
-
-    private void Start()
-    {
-        _rigidBody = GetComponent<Rigidbody>();
-    }
-
-    private void Update()
-    {
-        _input = Vector3.zero;
-        _input.z = Input.GetAxis("Vertical");
-        if (_input != Vector3.zero)
-=======
         Vector3 _moveDirection = new Vector3(0.0f, 0.0f, Input.GetAxis("Vertical"));
         if (_moveDirection.z != 0.0)
->>>>>>> Added a tutorial level which works locally
         {
             int dir = System.Math.Sign(_moveDirection.z);
             MoveZ(dir, Time.deltaTime);
