caa1f115862116de4b95d8e9fb4e10702f521427
ludvigarlebrink <ludvig.arlebrink@outlook.com>
2019-10-10 17:37:35 +0200

    HD pipeline and build settings for spectator

---
+++ QEGame/Assets/Editor/SpectatorBuild.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using UnityEditor;
+
+public class SpectatorBuild
+{
+    [MenuItem("Builds/Build and Run Spectator")]
+    public static void BuildGame()
+    {
+        string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
+        string[] levels = new string[] { "Assets/Spectator.unity" };
+
+        BuildPipeline.BuildPlayer(levels, path + "/Spectator.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+
+        Process proc = new Process();
+        proc.StartInfo.FileName = path + "/Spectator.exe";
+        proc.Start();
+    }
+}
