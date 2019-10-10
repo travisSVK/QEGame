bf95d61cfa1327e785afdf2e537e999c699941cb
travisko <marek.cern@gmail.com>
2019-10-10 09:25:29 +0200

    Changed character controller to rigidbody. Added black hole hazzard.

---
+++ QEGame/Assets/Scripts/Hazzards/BlackHoleBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHoleBehaviour : MonoBehaviour
+{
+    private float _pullFactor = 0.0f;
+    private Rigidbody _playerRigidBody;
+
+    public void FixedUpdate()
+    {
+        if (_playerRigidBody)
+        {
+            float distance = Vector3.Distance(transform.position, _playerRigidBody.transform.position);
+            Vector3 direction = transform.position - _playerRigidBody.transform.position;
+            float gravitationalPull = Mathf.Clamp(_pullFactor - distance, 0.0f, _pullFactor);
+            _playerRigidBody.MovePosition(_playerRigidBody.position + direction * gravitationalPull * Time.fixedDeltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collision collision)
+    {
+        PlayerTag playerTag = collision.collider.GetComponent<PlayerTag>();
+        if (playerTag)
+        {
+            _playerRigidBody = playerTag.GetComponent<Rigidbody>();
+        }
+    }
+
+    private void OnTriggerExit(Collision collision)
+    {
+        PlayerTag playerTag = collision.collider.GetComponent<PlayerTag>();
+        if (playerTag)
+        {
+            _playerRigidBody = null;
+        }
+    }
+
+    public void Start()
+    {
+        SphereCollider sphereCollider = GetComponent<SphereCollider>();
+        if (sphereCollider)
+        {
+            _pullFactor = sphereCollider.radius;
+        }
+        
+    }
+}
