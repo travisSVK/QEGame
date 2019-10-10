1e178231ba1f297e3ba588dedd619761351ab5e3
ludvigarlebrink <ludvig.arlebrink@outlook.com>
2019-10-10 13:15:29 +0200

    Start scene

---
+++ QEGame/Assets/Scripts/GoToScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToScene : MonoBehaviour
+{
+    public string nameOfScene = "";
+
+    private void Start()
+    {
+        SceneManager.LoadScene(nameOfScene);
+    }
+}
