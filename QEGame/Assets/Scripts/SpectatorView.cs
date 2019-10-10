2b23a4cb9fd0cc4f969e0ec9a54b78b537b28c90
ludvigarlebrink <ludvig.arlebrink@outlook.com>
2019-10-10 10:53:01 +0200

    Spectator

--- QEGame/Assets/Scripts/SpectatorView.cs
+++ QEGame/Assets/Scripts/SpectatorView.cs
@@ -4,5 +4,29 @@ using UnityEngine;
 
 public class SpectatorView : MonoBehaviour
 {
+    public Camera player1Camera = null;
 
+    public Camera player2Camera = null;
+
+    private void Start()
+    {
+        AudioListener audioListener0 = player1Camera.GetComponent<AudioListener>();
+        AudioListener audioListener1 = player1Camera.GetComponent<AudioListener>();
+
+        if (audioListener0)
+        {
+            Destroy(audioListener0);
+        }
+
+        if (audioListener1)
+        {
+            Destroy(audioListener1);
+        }
+
+        float height = (float)Screen.height;
+        float width = (float)Screen.width;
+
+        player1Camera.rect = new Rect(0.0f + (40 / width), 0.2f, 0.5f - (60 / width), 0.8f - (40 / height));
+        player2Camera.rect = new Rect(0.5f + (20 / width), 0.2f, 0.5f - (60 / width), 0.8f - (40 / height));
+    }
 }
