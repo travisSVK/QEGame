4b1a28a06a841eee5734b361055d26fd2fbf698d
travisko <marek.cern@gmail.com>
2019-10-25 03:16:36 +0200

    Enabling solar flares.

---
+++ QEGame/Assets/SolarFlareActivator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarFlareActivator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Activate()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+        }
+    }
+}
--- QEGame/Assets/SolarFlareBehaviour.cs
+++ QEGame/Assets/SolarFlareBehaviour.cs
@@ -7,7 +7,6 @@ public class SolarFlareBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.SetActive(false);
     }
 
     // Update is called once per frame
